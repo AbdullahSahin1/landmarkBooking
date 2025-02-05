cece9015b6ffde6b81d85acd1440344f5cb52575
AbdullahSahin1 <sahinabdullah2003@hotmail.com>
2025-02-05 14:13:59 +0300

    Dizi oluşturulup bilgiler diziye eklendi

---
+++ app/src/main/java/com/example/landmarkbook/Landmark.kt
@@ -0,0 +1,5 @@
+package com.example.landmarkbook
+
+class Landmark (val name: String, val country: String, val image: Int) {
+
+}
--- app/src/main/java/com/example/landmarkbook/MainActivity.kt
+++ app/src/main/java/com/example/landmarkbook/MainActivity.kt
@@ -6,14 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.landmarkbook.databinding.ActivityMainBinding
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var landmarkList: ArrayList<Landmark>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val pisa = Landmark("Pisa","Italy",R.drawable.pisa)
+        val london = Landmark("London Bridge","England",R.drawable.london)
+        val koln = Landmark("Koln","Germany",R.drawable.koln)
+        val effiel = Landmark("Effiel","France",R.drawable.effiel)
+
+        landmarkList.add(pisa)
+        landmarkList.add(london)
+        landmarkList.add(koln)
+        landmarkList.add(effiel)
 
     }
 }
